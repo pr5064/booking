c7357fd35d12895700b2e81c8998eb2defeb3b0b
pr5064 <pr5064@srmist.edu.in>
2025-02-13 17:46:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
-    // Event listener for modal open button
-    $(".btn-primary[data-toggle='modal']").click(function() {
-        $("#idea-modal").modal("show");
-    });
-
-    // Event listener for modal close button
-    $(".btn-secondary").click(function() {
-        $("#idea-modal").modal("hide");
-    });
-
-    // Event listener for 'Invest in an Idea' button
-    $(".btn-primary:contains('Invest in an Idea')").click(function() {
-        alert("Investment process coming soon!");
-    });
-
-    // Event listener for 'List Your Product or Idea' button
-    $(".call-to-action .btn-primary").click(function() {
-        alert("Feature coming soon!");
+    $("#invest-btn").click(function() {
+        window.location.href = "invest.html";
     });
 });
